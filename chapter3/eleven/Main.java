9d63d9c6c8cb4d9b4e4724cef388cc1d3fe64623
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2021-08-22 01:40:09 +0900

    add chapter3.eleven

---
+++ chapter3/eleven/Main.java
@@ -0,0 +1,9 @@
+package chapter3.eleven;
+
+public class Main {
+    public static void main(String[] args) {
+        Object a = new Object();
+        Object b = null;
+        System.out.println(a.equals(b));
+    }
+}
