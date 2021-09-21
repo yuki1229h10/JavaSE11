c41ef3818395649f1e47e92291dad81bcd4a9e8a
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2021-09-21 12:33:50 +0900

    change chapter2

---
+++ chapter2/ex16/Main.java
@@ -0,0 +1,10 @@
+package chapter2.ex16;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "abc";
+        String str2 = str.replace("a", "b");
+        System.out.println(str);
+        System.out.println(str2);
+    }
+}
