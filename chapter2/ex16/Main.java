044c309712c9ac7fd29956244903af72ba252701
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2021-09-21 12:51:00 +0900

    change chapter2

--- chapter2/ex16/Main.java
+++ chapter2/ex16/Main.java
@@ -6,5 +6,17 @@ public class Main {
         String str2 = str.replace("a", "b");
         System.out.println(str);
         System.out.println(str2);
+
+        String str3 = "abcde";
+        String str4 = str3.substring(1, 3).replace("b", "c");
+        System.out.println(str3);
+        System.out.println(str4);
+
+        String str5 = "abcde";
+        String str6 = str5.substring(1, 3);
+        String str7 = str6.replace("b", "c");
+        System.out.println(str5);
+        System.out.println(str6);
+        System.out.println(str7);
     }
 }
